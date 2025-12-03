f3d365c3e087126d8bd69f3c3b679b96a62e19fd
Zara Abigail Budiman <marinelamprey@gmail.com>
2025-12-03 13:26:04 +0700

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,9 +1,11 @@
+--making the new datawarehouse using the medallion method
+
 USE master;
 go
 
-if exists (select 1 from sys.databases where name = "DatawareHouse")
+if exists (select 1 from sys.databases where name = "DataWareHouse")
 begin
-  
+  alter database DataWareHouse set single_user with rollback immediate
 
 
 CREATE database DataWareHouse
