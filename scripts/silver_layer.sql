f4552037e163dad51ac233e28f14e021e6bd360a
Zara Abigail Budiman <marinelamprey@gmail.com>
2025-12-04 21:23:18 +0700

    Update silver_layer.sql

--- scripts/silver_layer.sql
+++ scripts/silver_layer.sql
@@ -95,6 +95,7 @@ SELECT cst_lastname from bronze.crm_cust_info
 where cst_lastname != TRIM(cst_lastname);
 
 -- cleaning the data
+TRUNCATE TABLE silver.crm_cust_info;
 INSERT INTO silver.crm_cust_info (
 	cst_id,
 	cst_key,
@@ -105,26 +106,38 @@ INSERT INTO silver.crm_cust_info (
 	cst_create_date)
 
 SELECT
-cst_id,
-cst_key,
-TRIM(cst_firstname) as cst_firstname,
-TRIM(cst_lastname) as cst_lastname,
+	cst_id,
+	cst_key,
+	TRIM(cst_firstname) as cst_firstname,
+	TRIM(cst_lastname) as cst_lastname,
 
-CASE WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
-	WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
-	ELSE 'n/a'
-END cst_marital_status,
+	CASE WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+		WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+		ELSE 'n/a'
+	END cst_marital_status,
 
-CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
-	WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
-	ELSE 'n/a'
-END cst_gndr,
+	CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+		WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+		ELSE 'n/a'
+	END cst_gndr,
 
-cst_create_date
-FROM
-(
+	cst_create_date
+	FROM
+	(
 	SELECT *,
 	ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) as flag_last
 	from bronze.crm_cust_info
-	WHERE cst_id IS NOT NULL)t
-	WHERE flag_last = 1;
+	WHERE cst_id IS NOT NULL
+)t WHERE flag_last = 1;
+
+-- checking quality of clean data in silver layer
+
+select cst_id, COUNT(*) 
+from silver.crm_cust_info 
+group by cst_id 
+having count(*) >1 or cst_id is NULL;
+
+select * from silver.crm_cust_info 
+
+SELECT cst_lastname from silver.crm_cust_info
+where cst_lastname != TRIM(cst_lastname);
